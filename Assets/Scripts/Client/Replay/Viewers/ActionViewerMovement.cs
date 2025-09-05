31c1eb365704e955f0e2bc4d970b16e7d29f3149
JinnFighter <jinnfighterwork@yandex.ru>
2025-09-05 17:10:26 +0300

    add: temp ui

---
+++ Assets/Scripts/Client/Replay/Viewers/ActionViewerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Logic.ActionEvents;
+
+namespace Client.Replay.Viewers
+{
+    public class ActionViewerMovement : IActionViewer
+    {
+        public IEnumerator<bool> Play(BattleInstanceClient battleInstance, ActionEvent actionEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
